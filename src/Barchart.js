b55fe42e07d284fb54c96b012814a6327c3ad203
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2018-01-13 23:12:53 +1300

    added 20yrs of data to Barchart.js

--- src/Barchart.js
+++ src/Barchart.js
@@ -38,17 +38,43 @@ const data = [
     {"year": 2008, "segment": "Renters", "value": 506700, pct: .29934424},
     {"year": 2008, "segment": "Owned", "value": 1117400, pct: .66012879},
     {"year": 2008, "segment": "Free", "value": 68700, pct: .04058605},
+    {"year": 2007, "segment": "Renters", "value": 494300, pct: .29556326},
+    {"year": 2007, "segment": "Owned", "value": 1109000, pct: .66311887},
+    {"year": 2007, "segment": "Free", "value": 69100, pct: .04131787},
+    {"year": 2006, "segment": "Renters", "value": 481200, pct: .29177783},
+    {"year": 2006, "segment": "Owned", "value": 1098600, pct: .66614116},
+    {"year": 2006, "segment": "Free", "value": 69400, pct: .04208101},
+    {"year": 2005, "segment": "Renters", "value": 469700, pct: .28877959},
+    {"year": 2005, "segment": "Owned", "value": 1088100, pct: .66898248},
+    {"year": 2005, "segment": "Free", "value": 68700, pct: .04223793},
+    {"year": 2004, "segment": "Renters", "value": 462900, pct: .28914985},
+    {"year": 2004, "segment": "Owned", "value": 1074000, pct: .67087263},
+    {"year": 2004, "segment": "Free", "value": 64000, pct: .03997751},
+    {"year": 2003, "segment": "Renters", "value": 454500, pct: .28950889},
+    {"year": 2003, "segment": "Owned", "value": 1056200, pct: .67278171},
+    {"year": 2003, "segment": "Free", "value": 59200, pct: .03770941},
+    {"year": 2002, "segment": "Renters", "value": 447200, pct: .28986259},
+    {"year": 2002, "segment": "Owned", "value": 1040900, pct: .6746824},
+    {"year": 2002, "segment": "Free", "value": 54600, pct: .0353902},
+    {"year": 2001, "segment": "Renters", "value": 441700, pct: .29024839},
+    {"year": 2001, "segment": "Owned", "value": 1029600, pct: .67656722},
+    {"year": 2001, "segment": "Free", "value": 50400, pct: .03311868},
+    {"year": 2000, "segment": "Renters", "value": 434900, pct: .28927764},
+    {"year": 2000, "segment": "Owned", "value": 1021100, pct: .67919383},
+    {"year": 2000, "segment": "Free", "value": 47400, pct: .03152854},
+    {"year": 1999, "segment": "Renters", "value": 416800, pct: .28183109},
+    {"year": 1999, "segment": "Owned", "value": 1013000, pct: .68496856},
+    {"year": 1999, "segment": "Free", "value": 49200, pct: .03326797},
+    {"year": 1998, "segment": "Renters", "value": 399600, pct: .27445055},
+    {"year": 1998, "segment": "Owned", "value": 1005600, pct: .69065934},
+    {"year": 1998, "segment": "Free", "value": 50900, pct: .03495879},
 ]
-
-1692700
-
-
 
 export default class Barchart extends Component {
   render() {
     return (
       <div>
-      <h3>Renters vs Owned vs Free Dwellings</h3>
+      <h3>Renters vs Owned vs Free Dwellings - 1998-2017</h3>
       <ORFrame
       size={[ 700,400 ]}
       data={data}
@@ -67,9 +93,8 @@ export default class Barchart extends Component {
             styleFn: d => ({ fill: d.color, stroke: "white" }),
             items: [
               { label: "Free", color: "#b3331d" },
-              { label: "Renters", color: "#00a2ce" },
               { label: "Owned", color: "#ffbf00" },
-
+              { label: "Renters", color: "#00a2ce" },
             ]
           }
         ]
